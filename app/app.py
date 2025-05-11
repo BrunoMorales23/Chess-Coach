6ca4ecc5d19c90afb9de692b41e21ce72495ae5f
Bruno Morales <brunomorales0323@outlook.es>
2025-05-11 18:06:16 -0300

    Responsive design added

--- app/app.py
+++ app/app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, session, request, send_file, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
 import os
+import json
+
 
 app = Flask(__name__)
+config_path = 'C:/Users/MarsuDIOS666/Desktop/CHESS-COACH-MAIN/config.json'
+config_data = {}
 app.secret_key = '123'
 browser = None
 next_button = None
@@ -16,24 +21,33 @@ prev_button = None
 output_path = './static/images/ss.webp'
 streaming_game = False
 filename_value = ''
-root = "C:/Users/MarsuDIOS666/Desktop/Chess Coach App/app/static/images/ss.webp"
-target_root = "C:/Users/MarsuDIOS666/Desktop/Chess Coach App/app/static/images/"
+#root = "C:/Users/MarsuDIOS666/Desktop/Chess Coach App/app/static/images/ss.webp"
+#target_root = "C:/Users/MarsuDIOS666/Desktop/Chess Coach App/app/static/images/"
+root = ""
+target_root = ""
 
 #inicializar explorador + vista inicial
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
     global browser
-    chrome_options = Options()
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    #chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--ignore-certificate-errors")
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
-    browser = webdriver.Chrome(options=chrome_options) #service=service)
-    login(user="chesscoachselenium@gmail.com" , password="ChessCoach2303@")
+    if request.method == 'POST':
+        #volver a la vista inicial
+        return render_template('initialize.html')
+    else:
+        get_config_file(config_path)
+        set_initial_paths(priv_env=True)
+        chrome_options = Options()
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option('useAutomationExtension', False)
+        browser = webdriver.Chrome(options=chrome_options) #service=service)
+        login(user="XXX@gmail.com" , password="XXX")
     return render_template('initialize.html')
 
 #lanzar la búsqueda del estado del tablero + inicializar vista de tablero
@@ -43,36 +57,53 @@ def home():
     global streaming_game
     action = request.form.get("action")
     match (action, streaming_game):
-        case ("next", True):
-            next_move(next=True)
-            time.sleep(0.1)
-            get_screenshot()
-            render_value = "dynamicIndex.html"
+        #case ("next", True):
+        #    next_move(next=True)
+        #    time.sleep(0.1)
+        #    get_screenshot()
+        #    render_value = "dynamicIndex.html"
         case ("next" , False):
             next_move(next=True)
             time.sleep(0.1)
             get_screenshot()
             render_value = "index.html"
-        case ("previous", True):
-            next_move(next=False)
-            time.sleep(0.1)
-            get_screenshot()
-            render_value = "dynamicIndex.html"
+        #case ("previous", True):
+        #    next_move(next=False)
+        #    time.sleep(0.1)
+        #    get_screenshot()
+        #    render_value = "dynamicIndex.html"
         case ("previous" , False):
             next_move(next=False)
             time.sleep(0.1)
             get_screenshot()
             render_value = "index.html"
+        case ("switch", False):
+            config_xpath_code = "//button[contains(@id,'board-controls-settings')]"
+            switch_xpath_code = "//button[contains(@id,'board-controls-flip')]"
+            config_xpath_code = browser.find_element("xpath", config_xpath_code)
+            switch_xpath_code = browser.find_element("xpath", switch_xpath_code)
+            action_steps = ActionChains(browser)
+            action_steps.move_to_element(config_xpath_code).perform()
+            switch_xpath_code.click()
+            time.sleep(0.1)
+            get_screenshot()
+            render_value = "index.html"
         case _:
-            game_code = request.form.get('game-code')
-            move_num = request.form.get('move-number')
-            url = f"https://www.chess.com/game/live/{game_code}?move={move_num}"
-            session['move-number'] = move_num
-            session['game-code'] = game_code
-            session['url'] = url
             url = session.get('url')
-            browser.get(url)
-            live_game = get_screenshot()
+            if url == None or url == 'None':
+                game_code = request.form.get('game-code')
+                move_num = request.form.get('move-number')
+                url = f"https://www.chess.com/game/live/{game_code}?move={move_num}"
+                session['move-number'] = move_num
+                session['game-code'] = game_code
+                session['url'] = url
+                url = session.get('url')
+                browser.get(url)
+                live_game = get_screenshot()
+            else:
+                browser.get(url)
+                live_game = get_screenshot()
+            
             if live_game == True:
                 streaming_game = True
                 render_value = "dynamicIndex.html"
@@ -82,16 +113,18 @@ def home():
     return render_template(render_value)
 
 #establecer imagen dinámica
-@app.route('/dynamic-index', methods=['GET', 'POST', 'DELETE'])
+@app.route('/game-live', methods=['GET', 'POST', 'DELETE'])
 def get_image():
     if request.method == 'POST':
         global filename_value
+        time.sleep(0.1)
         get_screenshot()
         data = request.get_json()
         filename = data.get("filename")
         os.rename(root, target_root+filename)
         filename_value = "/static/images/"+filename
-        time.sleep(10)
+        #filename_value = target_root+filename
+        time.sleep(5)
         return send_file(filename_value, mimetype='image/webp')
     elif request.method == 'DELETE':
         data = request.get_json()
@@ -102,15 +135,6 @@ def get_image():
     elif request.method == 'GET':
         return jsonify({"filename": filename_value}), 200
 
-
-#volver a la vista inicial
-@app.route('/', methods=['POST'])
-def reset():
-    return render_template('initialize.html')
-
-
-#definir URL y establecer en las variables de sesión los valores de movimiento, código de partida y url
-@app.route('/game')
 def next_move(next):
     global next_button
     global prev_button
@@ -126,21 +150,29 @@ def next_move(next):
         prev_button.click()
     return
 
-#acceder al explorador en el movimiento y juego declarado para obtener la instancia actual del tablero
-@app.route('/game')
 def get_screenshot():
     try:
+        #primero se intenta identificar si es un juego en vivo, buscando el botón de la review  (que aparece en juegos finalizados)
         xpath_code = "//div[contains(@class,'game-review-buttons-component')]"
-        WebDriverWait(browser, 5).until(
-        EC.visibility_of_element_located((By.XPATH, xpath_code))
-        )
+        web_element_exists = wait_web_element(xpath_code, 10)
+        #con esto validamos que si nos paramos en una partida finalizada y no asignamos numero de movimiento, en caso de llegar a la instancia final, cerramos
+        #la vista que nos arroja los resultados de la partida.
+        if web_element_exists == False:
+            raise Exception(f"Unable to locate web element: {xpath_code}")
+        xpath_code = "//div[contains(@class,'game-over-modal-content')]//button[contains(@aria-label,'Close')]"
+        web_element_exists = wait_web_element(xpath_code, 1)
+        if web_element_exists == True: 
+            close_end_game_button = browser.find_element(By.XPATH, xpath_code)
+            close_end_game_button.click()
+
         xpath_code = "//wc-chess-board[contains(@id,'board-single') and contains(@class,'board')]"
         board = browser.find_element("xpath", xpath_code)
         board.screenshot('./static/images/ss.webp')
         live_game = False
+        #en caso de que tras buscar por 3 segundos, no exista, entendemos que es un juego en vivo
     except TimeoutException:
         xpath_code = "//wc-chess-board[contains(@id,'board-single') and contains(@class,'board')]"
-        board = browser.find_element("xpath", "//wc-chess-board[contains(@id,'board-single') and contains(@class,'board')]")
+        board = browser.find_element("xpath", xpath_code)
         board.screenshot(output_path)
         live_game = True
         return live_game
@@ -156,15 +188,46 @@ def login(user, password):
     xpath_code = "//button[contains(text(),'Log In')]"
     login_button = browser.find_element(By.XPATH, xpath_code)
     login_button.click()
+    xpath_code = "//a[contains(text(),'ChessCoachSelenium')]"
+    web_element_exists = wait_web_element(xpath_code, 10)
+    if not web_element_exists: raise Exception(f"Unable to locate web element: {xpath_code}")
+        
+def set_initial_paths(priv_env):
+    global root
+    global target_root
+
+    if priv_env == True:
+        chess_coach_path = os.path.join(os.path.expanduser("~"), "Chess-Coach")
+        root = chess_coach_path+"/app/static/images/ss.webp"
+        target_root = chess_coach_path+"/app/static/images/"
+        root = root.replace("\\", "/")
+        target_root = target_root.replace("\\", "/")
+    else:
+        root = "C:/Users/bmorales/OneDrive - rmrconsultores.com/Escritorio/Chess-Coach/app/static/images/ss.webp"
+        target_root = "C:/Users/bmorales/OneDrive - rmrconsultores.com/Escritorio/Chess-Coach/app/static/images/"
+
+def validate_web_element(xpath):
+    web_element = browser.find_elements(By.XPATH, xpath)
+    return len(web_element) > 0
+
+def wait_web_element(xpath, time):
     try:
-        xpath_code = "//a[contains(text(),'ChessCoachSelenium')]"
-        WebDriverWait(browser, 10).until(
-        EC.visibility_of_element_located((By.XPATH, xpath_code))
-        )
-        print("LogIn Successful")
-        pass
+        WebDriverWait(browser, time).until(
+            EC.visibility_of_element_located((By.XPATH, xpath))
+            )
+        return True
     except TimeoutException:
-        raise Exception("Unable to LogIn, failed at def 'login'")
+        return False
+    
+def get_config_file(path):
+    global config_data
+    with open(path) as config:
+        config_data = json.load(config)
+
+def save_config_data(path):
+    global config_data
+    with open(path, "w") as config:
+        json.dump(config_data, config, indent=4)
     
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- app/static/script.js
+++ app/static/script.js
@@ -1,38 +1,61 @@
-setInterval(() => {
-    const timestamp = Date.now();
-    const filename = `ss_${timestamp}.webp`;
+let intervalId = null;
+let isPaused = false;
 
-    fetch('/dynamic-index', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ filename: filename })
-    })
-    .then(() => {
-        console.log("POST Primer paso done! Nombre de Archivo generado:", filename);
+function loop() {
+    setInterval(() => {
+        const timestamp = Date.now();
+        const filename = `ss_${timestamp}.webp`;
 
-        return fetch('/dynamic-index', {
-            method: 'GET'
-        });
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("GET Segundo paso done! Nombre del archivo recibido del backend:", data.filename);
-
-        const img = document.getElementById('live-image');
-        img.src = data.filename;
-
-        console.log("Tablero actualizado!")
-
-        return fetch('/dynamic-index', {
-            method: 'DELETE',
+        fetch('/game-live', {
+            method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ filename: filename })
+        })
+        .then(() => {
+            console.log("POST Primer paso done! Nombre de Archivo generado:", filename);
+
+            return fetch('/game-live', {
+                method: 'GET'
+            });
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("GET Segundo paso done! Nombre del archivo recibido del backend:", data.filename);
+
+            const img = document.getElementById('live-image');
+            img.src = data.filename;
+
+            console.log("Tablero actualizado!")
+
+            return fetch('/game-live', {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ filename: filename })
+            });
+        })
+        .then(() => {
+            console.log("DELETE Tercer paso done!, Ciclo completo realizado!");
+        })
+        .catch(error => {
+            console.error("Error en alguna parte del ciclo:", error);
         });
-    })
-    .then(() => {
-        console.log("DELETE Tercer paso done!, Ciclo completo realizado!");
-    })
-    .catch(error => {
-        console.error("Error en alguna parte del ciclo:", error);
-    });
-}, 3000);
+    },);
+}
+
+intervalId = setInterval(loop, 3500);
+
+document.getElementById('toggle-button').addEventListener('click', () => {
+    if (isPaused) {
+        intervalId = setInterval(ejecutarCiclo, 3500);
+        document.getElementById('toggle-button').textContent = 'Pausar';
+        isPaused = false;
+        console.log("Intervalo reanudado");
+    } else {
+        // Pausar
+        clearInterval(intervalId);
+        intervalId = null;
+        document.getElementById('toggle-button').textContent = 'Reanudar';
+        isPaused = true;
+        console.log("Intervalo pausado");
+    }
+});
